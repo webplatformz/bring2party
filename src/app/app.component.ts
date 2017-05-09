6ee56ae5b5ace9df88fdb7a2a7562062f9ad0593
Marco Steybe <sty@zuehlke.com>
2017-05-09 15:19:44 +0200

    add material purple theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,7 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
 
   constructor() {
-  }
-
-  ngOnInit() {
   }
 }
