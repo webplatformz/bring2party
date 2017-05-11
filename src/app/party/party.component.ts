530de6165abef99247ca1d296c65b7fa0fce68be
Marco Steybe <sty@zuehlke.com>
2017-05-11 16:06:42 +0200

    added date and time picker

--- src/app/party/party.component.ts
+++ src/app/party/party.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit }        from '@angular/core';
-import { ActivatedRoute, Params }   from '@angular/router';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
+import mdDateTimePicker from 'md-date-time-picker';
 
-import { Party }                    from "../shared/party";
-import { User }                     from "../shared/user";
-import { Item }                     from "../shared/item";
-import { PartyService }             from "../shared/party.service";
+import {Party}                    from "../shared/party";
+import {User}                     from "../shared/user";
+import {Item}                     from "../shared/item";
+import {PartyService}             from "../shared/party.service";
+
+declare const moment: any;
 
 @Component({
   selector: 'app-party',
@@ -15,10 +18,47 @@ export class PartyComponent implements OnInit {
 
   party: Party;
 
-  constructor(
-    private route: ActivatedRoute,
-    private partyService: PartyService
-  ) { }
+  datepicker: mdDateTimePicker;
+  timepicker: mdDateTimePicker;
+
+  constructor(private route: ActivatedRoute,
+              private partyService: PartyService) {
+    this.datepicker = new mdDateTimePicker({
+      type: 'date',
+      past: moment(),
+      future: moment().add(10, 'years')
+    });
+    this.timepicker = new mdDateTimePicker({
+      type: 'time',
+      mode: true
+    });
+  }
+
+  onDateSelected(date) {
+    let newDate = new Date(this.party.date.getTime());
+    newDate.setFullYear(date.get('year'));
+    newDate.setMonth(date.get('month'));
+    newDate.setDate(date.get('date'));
+    this.party.date = newDate;
+  }
+
+  onTimeSelected(time) {
+    let newDate = new Date(this.party.date.getTime());
+    newDate.setHours(time.get('hour'), time.get('minute'), 0, 0);
+    this.party.date = newDate;
+  }
+
+  @ViewChild('datepickerRef') set initDatepicker(datepickerRef: ElementRef) {
+    if (datepickerRef && datepickerRef.nativeElement) {
+      this.datepicker.trigger = datepickerRef.nativeElement;
+    }
+  }
+
+  @ViewChild('timepickerRef') set initTimepicker(timepickerRef: ElementRef) {
+    if (timepickerRef && timepickerRef.nativeElement) {
+      this.timepicker.trigger = timepickerRef.nativeElement;
+    }
+  }
 
   ngOnInit() {
     this.route.params
